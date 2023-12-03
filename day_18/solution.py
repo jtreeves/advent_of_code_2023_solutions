133e284147f1f47136ee19f210de60d8f932f042
jtreeves <jackson.t.reeves@gmail.com>
2023-12-02 23:39:54 -0500

    Add placeholder solution functions and import them into the main runner

---
+++ day_18/solution.py
@@ -0,0 +1,14 @@
+import time
+from utils.extract_data_from_file import extract_data_from_file
+from utils.SolutionResults import SolutionResults
+
+
+def solution(is_official: bool) -> SolutionResults:
+    start_time = time.time()
+    data = extract_data_from_file(18, is_official)
+    part_1 = 1 if data else 0
+    part_2 = 2 if data else 0
+    end_time = time.time()
+    execution_time = end_time - start_time
+    results = SolutionResults(18, part_1, part_2, execution_time)
+    return results
