9b50aa6690be1d9fd13399b350fe4ca1476aff95
jtreeves <jackson.t.reeves@gmail.com>
2023-12-04 01:18:45 -0500

    Solve part 2 for day 4

--- day_4/solution.py
+++ day_4/solution.py
@@ -12,10 +12,11 @@ class Card:
         self.id = self.determine_id(initial_sections[0])
         self.winning_numbers = self.determine_numbers_list(number_sections[0])
         self.provided_numbers = self.determine_numbers_list(number_sections[1])
+        self.matches = self.determine_matches()
         self.points = self.calculate_points()
 
     def __repr__(self) -> str:
-        return f"Card {self.id}: {self.winning_numbers} | {self.provided_numbers} -> {self.points}"
+        return f"Card {self.id}: {self.winning_numbers} | {self.provided_numbers} -> {self.matches} matches and {self.points} points"
 
     def determine_id(self, introduction: str) -> int:
         string_id = introduction.split()[1]
@@ -27,21 +28,31 @@ class Card:
         numbers_list = list(map(int, filter(lambda x: x != "", string_numbers)))
         return numbers_list
 
-    def calculate_points(self) -> int:
+    def determine_matches(self) -> int:
         matches = 0
         for provided_number in self.provided_numbers:
             if provided_number in self.winning_numbers:
                 matches += 1
-        if matches > 0:
-            points = 2**(matches - 1)
+        return matches
+
+    def calculate_points(self) -> int:
+        if self.matches > 0:
+            points = 2**(self.matches - 1)
         else:
             points = 0
         return points
+
+    def determine_ids_of_copies(self) -> List[int]:
+        ids: List[int] = []
+        for value in range(self.matches):
+            ids.append(self.id + value + 1)
+        return ids
 
 
 class Pile:
     def __init__(self, description: str) -> None:
         self.cards = self.create_cards(description)
+        self.card_counts = self.initialize_counts_of_cards()
         self.total_points = self.calculate_total_points()
 
     def __repr__(self) -> str:
@@ -65,13 +76,32 @@ class Pile:
             total_points += card.points
         return total_points
 
+    def initialize_counts_of_cards(self) -> dict[int, int]:
+        card_counts: dict[int, int] = {}
+        for card in self.cards:
+            card_counts[card.id] = 1
+        return card_counts
+
+    def increment_card_counts_iteratively(self) -> None:
+        for card in self.cards:
+            ids_of_copies = card.determine_ids_of_copies()
+            for id in ids_of_copies:
+                self.card_counts[id] += self.card_counts[card.id]
+
+    def determine_total_card_count(self) -> int:
+        total = 0
+        for value in self.card_counts.values():
+            total += value
+        return total
+
 
 def solve_problem(is_official: bool) -> SolutionResults:
     start_time = time.time()
     data = extract_data_from_file(4, is_official)
     pile = Pile(data)
+    pile.increment_card_counts_iteratively()
     part_1 = pile.total_points
-    part_2 = 2 if data else 0
+    part_2 = pile.determine_total_card_count()
     end_time = time.time()
     execution_time = end_time - start_time
     results = SolutionResults(4, part_1, part_2, execution_time)
