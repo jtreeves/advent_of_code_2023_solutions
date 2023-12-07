1c5bc2a0f952924bd66c21b63159ee7da7435a19
jtreeves <jackson.t.reeves@gmail.com>
2023-12-07 03:00:01 -0500

    Solve part 2 for day 7

--- day_7/solution.py
+++ day_7/solution.py
@@ -5,6 +5,7 @@ from utils.get_list_of_lines import get_list_of_lines
 from utils.SolutionResults import SolutionResults
 
 card_orders = ["2", "3", "4", "5", "6", "7", "8", "9", "T", "J", "Q", "K", "A"]
+joker_card_orders = ["J", "2", "3", "4", "5", "6", "7", "8", "9", "T", "Q", "K", "A"]
 
 
 class Hand:
@@ -13,6 +14,7 @@ class Hand:
         self.cards = elements[0]
         self.bid = int(elements[1])
         self.value = self.calculate_value()
+        self.joker_value = self.calculate_value_with_jokers()
 
     def __repr__(self) -> str:
         return f"{self.cards}: {self.bid} -> {self.value}"
@@ -28,6 +30,51 @@ class Hand:
                     matches[card] += 1
                 else:
                     matches[card] = 2
+        if len(matches.items()) == 0:
+            value = 1
+        else:
+            for inner_value in matches.values():
+                value += inner_value ** inner_value
+        return value
+
+    def calculate_value_with_jokers(self) -> int:
+        value = 0
+        sorted_cards = sorted(self.cards)
+        matches: dict[str, int] = {}
+        for index in range(len(sorted_cards) - 1):
+            if sorted_cards[index] == sorted_cards[index + 1]:
+                card = sorted_cards[index]
+                if matches.get(card):
+                    matches[card] += 1
+                else:
+                    matches[card] = 2
+        if "J" in sorted_cards:
+            if len(matches.items()) == 0:
+                matches["J"] = 2
+            else:
+                if not matches.get("J"):
+                    temp_key = ""
+                    temp_value = 0
+                    for [k, v] in matches.items():
+                        if v > temp_value:
+                            temp_value = v
+                            temp_key = k
+                    highest_match = temp_value + 1
+                    matches[temp_key] = highest_match
+                else:
+                    if len(matches.items()) == 1:
+                        if matches["J"] != 5:
+                            matches["J"] += 1
+                    else:
+                        temp_key = ""
+                        temp_value = 0
+                        for [k, v] in matches.items():
+                            if v > temp_value and k != "J":
+                                temp_value = v
+                                temp_key = k
+                        highest_match = temp_value + matches["J"]
+                        matches[temp_key] = highest_match
+                        del matches["J"]
         if len(matches.items()) == 0:
             value = 1
         else:
@@ -57,9 +104,19 @@ class CardSet:
     def order_hands(self) -> List[Hand]:
         return sorted(self.hands, key=lambda x: (x.value, card_orders.index(x.cards[0]), card_orders.index(x.cards[1]), card_orders.index(x.cards[2]), card_orders.index(x.cards[3]), card_orders.index(x.cards[4])))
 
+    def order_hands_with_jokers(self) -> List[Hand]:
+        return sorted(self.hands, key=lambda x: (x.joker_value, joker_card_orders.index(x.cards[0]), joker_card_orders.index(x.cards[1]), joker_card_orders.index(x.cards[2]), joker_card_orders.index(x.cards[3]), joker_card_orders.index(x.cards[4])))
+
     def calculate_winnings(self) -> int:
         winnings = 0
         ordered_hands = self.order_hands()
+        for index in range(len(ordered_hands)):
+            winnings += (index + 1) * ordered_hands[index].bid
+        return winnings
+
+    def calculate_winnings_with_jokers(self) -> int:
+        winnings = 0
+        ordered_hands = self.order_hands_with_jokers()
         for index in range(len(ordered_hands)):
             winnings += (index + 1) * ordered_hands[index].bid
         return winnings
@@ -70,7 +127,7 @@ def solve_problem(is_official: bool) -> SolutionResults:
     data = extract_data_from_file(7, is_official)
     cards = CardSet(data)
     part_1 = cards.calculate_winnings()
-    part_2 = 2 if data else 0
+    part_2 = cards.calculate_winnings_with_jokers()
     end_time = time.time()
     execution_time = end_time - start_time
     results = SolutionResults(7, part_1, part_2, execution_time)
