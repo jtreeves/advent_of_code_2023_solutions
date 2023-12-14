fe9b92ca822a71c31a8e017562121fb5bf277c24
Jackson Reeves <jackson.reeves@slalom.com>
2023-12-13 23:21:59 -0500

    Attempt part 1 of day 13

--- day_12/solution.py
+++ day_12/solution.py
@@ -26,16 +26,26 @@ class ConditionsRecord:
             set_subsection = self.original_conditions[0:next_unknown_index]
             first_possibility = set_subsection + "#"
             second_possibility = set_subsection + "."
-            possible_beginnings.extend([first_possibility, second_possibility])
+            possibilities = [first_possibility, second_possibility]
+            print('INITIAL POSSIBILITIES:', possibilities)
+            for possibility in possibilities:
+                meets_criteria = self.check_if_meets_criteria(possibility)
+                if meets_criteria:
+                    possible_beginnings.append(possibility)
         while len(possible_beginnings) != 0:
             possible_beginning = possible_beginnings.pop()
+            print('POSSIBLE BEGINNING:', possible_beginning)
             current_index = len(possible_beginning)
             next_unknown_index = self.find_next_unknown_condition(current_index)
             if next_unknown_index != -1:
                 set_subsection = possible_beginning + self.original_conditions[current_index:next_unknown_index]
                 first_possibility = set_subsection + "#"
                 second_possibility = set_subsection + "."
-                possible_beginnings.extend([first_possibility, second_possibility])
+                possibilities = [first_possibility, second_possibility]
+                for possibility in possibilities:
+                    meets_criteria = self.check_if_meets_criteria(possibility)
+                    if meets_criteria:
+                        possible_beginnings.append(possibility)
             else:
                 acceptable_arrangements += 1
         return acceptable_arrangements
@@ -44,24 +54,41 @@ class ConditionsRecord:
         next_index = self.original_conditions.find("?", start_index)
         return next_index
 
-    def find_next_damaged_group(self, start_index: int) -> dict[str, int]:
-        next_damaged_index = self.original_conditions.find("#", start_index)
-        later_operational_index = self.original_conditions.find(".", next_damaged_index)
-        damaged_group_length = later_operational_index - next_damaged_index
+    def find_next_damaged_group(self, possible_description: str, start_index: int) -> dict[str, int]:
+        print('INSIDE FIND NEXT DAMAGED GROUP')
+        print('START INDEX:', start_index)
+        next_damaged_index = possible_description.find("#", start_index)
+        next_unknown_index = possible_description.find("?", start_index)
+        least_starting_index = next_damaged_index if next_damaged_index < next_unknown_index else next_unknown_index
+        later_operational_index = possible_description.find(".", least_starting_index)
+        later_unknown_index = possible_description.find("?", least_starting_index)
+        least_ending_index = later_operational_index if later_operational_index < later_unknown_index else later_unknown_index
+        damaged_group_length = least_ending_index - least_starting_index if least_ending_index != least_starting_index else 1
         damaged_group = {
-            "start_index": next_damaged_index,
-            "length": damaged_group_length
+            "length": damaged_group_length,
+            "end_index": least_ending_index
         }
         return damaged_group
 
-    def check_if_meets_criteria(self, description: str) -> bool:
+    def check_if_meets_criteria(self, possible_beginning: str) -> bool:
+        print('*** BEGIN NEW CHECK')
         meets_criteria = True
-        start_index = 0
-        while meets_criteria:
-            for group_length in self.contiguous_groups:
-                next_damaged_group = self.find_next_damaged_group(start_index)
-                if next_damaged_group["length"] != group_length:
-                    meets_criteria = False
+        possible_description = possible_beginning + self.original_conditions[len(possible_beginning):]
+        print('DESCRIPTION TO CHECK:', possible_description)
+        description_index_to_check = 0
+        group_index_to_check = 0
+        while meets_criteria and description_index_to_check < len(self.original_conditions) and group_index_to_check < len(self.contiguous_groups):
+            next_damaged_group = self.find_next_damaged_group(possible_description, description_index_to_check)
+            print('NEXT DAMAGED GROUP:', next_damaged_group)
+            print('GROUP INDEX TO CHECK:', group_index_to_check)
+            if self.contiguous_groups[group_index_to_check] < next_damaged_group["length"]:
+                meets_criteria = False
+            else:
+                description_index_to_check = next_damaged_group["end_index"]
+                group_index_to_check += 1
+        if group_index_to_check != len(self.contiguous_groups):
+            meets_criteria = False
+        print('MEETS CRITERIA?', meets_criteria)
         return meets_criteria
 
 
@@ -80,7 +107,8 @@ def solve_problem(is_official: bool) -> SolutionResults:
     start_time = time.time()
     data = extract_data_from_file(12, is_official)
     record_descriptions = get_list_of_lines(data)
-    records = RecordsCollection(record_descriptions)
+    collection = RecordsCollection(record_descriptions)
+    print('FIRST POSSIBILITIES:', collection.records[1].count_acceptable_arrangements())
     part_1 = 1 if data else 0
     part_2 = 2 if data else 0
     end_time = time.time()
--- day_13/solution.py
+++ day_13/solution.py
@@ -1,12 +1,93 @@
 import time
+from typing import List
 from utils.extract_data_from_file import extract_data_from_file
 from utils.SolutionResults import SolutionResults
+
+
+class Pattern:
+    def __init__(self, notes: str) -> None:
+        self.notes = notes
+        self.rows = self.determine_rows()
+        self.columns = self.determine_columns()
+
+    def determine_rows(self) -> List[str]:
+        rows = self.notes.split("\n")
+        return rows
+
+    def determine_columns(self) -> List[str]:
+        raw_columns = [list(x) for x in zip(*self.rows)]
+        columns = [''.join(column) for column in raw_columns]
+        return columns
+
+    def calculate_value_of_symmetry(self) -> int:
+        horizontal = self.find_horizontal_line_of_symmetry()
+        vertical = self.find_vertical_line_of_symmetry()
+        if horizontal != 0:
+            value_of_symmetry = 100 * (horizontal + 1)
+        else:
+            value_of_symmetry = vertical + 1
+        return value_of_symmetry
+
+    def find_horizontal_line_of_symmetry(self) -> int:
+        return self.find_line_of_symmetry(self.rows)
+
+    def find_vertical_line_of_symmetry(self) -> int:
+        return self.find_line_of_symmetry(self.columns)
+
+    def find_line_of_symmetry(self, dimension: List[str]) -> int:
+        line_index = 0
+        potential_indices_of_symmetry = self.find_potential_indices_of_symmetry(dimension)
+        for index in potential_indices_of_symmetry:
+            fully_symmetric = self.check_if_fully_symmetric(index, dimension)
+            if fully_symmetric:
+                line_index = index
+        return line_index
+
+    def find_potential_indices_of_symmetry(self, dimension: List[str]) -> List[int]:
+        potential_indices_of_symmetry: List[int] = []
+        for index in range(len(dimension) - 1):
+            if dimension[index] == dimension[index + 1]:
+                potential_indices_of_symmetry.append(index)
+        return potential_indices_of_symmetry
+
+    def check_if_fully_symmetric(self, potential_index: int, dimension: List[str]) -> bool:
+        fully_symmetric = True
+        preceding_index = potential_index - 1
+        succeeding_index = potential_index + 2
+        while fully_symmetric and preceding_index >= 0 and succeeding_index < len(dimension):
+            if dimension[preceding_index] != dimension[succeeding_index]:
+                fully_symmetric = False
+            else:
+                preceding_index -= 1
+                succeeding_index += 1
+        return fully_symmetric
+
+
+class Collection:
+    def __init__(self, description: str) -> None:
+        self.description = description
+        self.patterns = self.determine_patterns()
+
+    def determine_patterns(self) -> List[Pattern]:
+        patterns: List[Pattern] = []
+        descriptions = self.description.split("\n\n")
+        for notes in descriptions:
+            pattern = Pattern(notes)
+            patterns.append(pattern)
+        return patterns
+
+    def calculate_total_values_of_symmetry(self) -> int:
+        total = 0
+        for pattern in self.patterns:
+            total += pattern.calculate_value_of_symmetry()
+        return total
 
 
 def solve_problem(is_official: bool) -> SolutionResults:
     start_time = time.time()
     data = extract_data_from_file(13, is_official)
-    part_1 = 1 if data else 0
+    collection = Collection(data)
+    part_1 = collection.calculate_total_values_of_symmetry()
     part_2 = 2 if data else 0
     end_time = time.time()
     execution_time = end_time - start_time
