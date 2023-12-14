5f25095843c37010510e42816ba817ddfa16e197
Jackson Reeves <jackson.reeves@slalom.com>
2023-12-14 02:17:28 -0500

    Solve part 1 for day 14

--- day_14/solution.py
+++ day_14/solution.py
@@ -1,4 +1,5 @@
 import time
+from utils.get_list_of_lines import get_list_of_lines
 from utils.extract_data_from_file import extract_data_from_file
 from utils.SolutionResults import SolutionResults
 
@@ -6,7 +7,20 @@ from utils.SolutionResults import SolutionResults
 def solve_problem(is_official: bool) -> SolutionResults:
     start_time = time.time()
     data = extract_data_from_file(14, is_official)
-    part_1 = 1 if data else 0
+    rows = get_list_of_lines(data)
+    columns = [''.join(column) + "#" for column in [list(reversed(x)) for x in zip(*rows)]]
+    total = 0
+    for column in columns:
+        last_cube = 0
+        cube = column.find("#")
+        while cube > -1:
+            sub_column = column[last_cube:cube]
+            spheres = sub_column.count("O")
+            for sphere in range(spheres):
+                total += cube - sphere
+            last_cube = cube
+            cube = column.find("#", cube + 1)
+    part_1 = total
     part_2 = 2 if data else 0
     end_time = time.time()
     execution_time = end_time - start_time
