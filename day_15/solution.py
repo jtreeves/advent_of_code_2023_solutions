11f9b62fe212e3bb3c255f6527eae4b08303d234
Jackson Reeves <jackson.reeves@slalom.com>
2023-12-15 00:20:14 -0500

    Apply hash algorithm to solve part 1 for day 15

--- day_15/solution.py
+++ day_15/solution.py
@@ -3,10 +3,23 @@ from utils.extract_data_from_file import extract_data_from_file
 from utils.SolutionResults import SolutionResults
 
 
+def apply_hash_algorithm(input: str) -> int:
+    value = 0
+    for character in input:
+        value += ord(character)
+        value *= 17
+        value %= 256
+    return value
+
+
 def solve_problem(is_official: bool) -> SolutionResults:
     start_time = time.time()
     data = extract_data_from_file(15, is_official)
-    part_1 = 1 if data else 0
+    steps = data.split(",")
+    hash_value = 0
+    for step in steps:
+        hash_value += apply_hash_algorithm(step)
+    part_1 = hash_value
     part_2 = 2 if data else 0
     end_time = time.time()
     execution_time = end_time - start_time
