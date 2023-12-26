6e66d5ec5284726a0c2e94c904d5cc7463f22cb4
Jackson Reeves <jackson.reeves@slalom.com>
2023-12-26 00:06:53 -0500

    Solve part 1 for day 24

--- day_23/solution.py
+++ day_23/solution.py
@@ -1,5 +1,5 @@
 import time
-from typing import List, Tuple, Sequence
+from typing import Tuple, Sequence
 from utils.get_list_of_lines import get_list_of_lines
 from utils.extract_data_from_file import extract_data_from_file
 from utils.SolutionResults import SolutionResults
@@ -12,18 +12,6 @@ def create_hiking_map(description: str) -> dict[Tuple[int, int], str]:
         for column in range(len(rows[0])):
             hiking_map[(column, row)] = rows[row][column]
     return hiking_map
-
-
-def find_traversable_neighbors(position: Tuple[int, int], hiking_map: dict[Tuple[int, int], str]) -> Sequence[Tuple[int, int]]:
-    traversable_neighbors: Sequence[Tuple[int, int]] = []
-    x, y = position
-    for dx, dy in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
-        nx, ny = x + dx, y + dy
-        next_position = hiking_map.get((nx, ny))
-        if next_position and next_position != "#":
-            if (nx < x and next_position != ">") or (nx > x and next_position != "<") or (ny < y and next_position != "v") or (ny > y and next_position != "^"):
-                traversable_neighbors.append((nx, ny))
-    return traversable_neighbors
 
 
 def find_start_and_end(hiking_map: dict[Tuple[int, int], str]) -> dict[str, Tuple[int, int]]:
@@ -40,31 +28,49 @@ def find_start_and_end(hiking_map: dict[Tuple[int, int], str]) -> dict[str, Tupl
     return start_and_end
 
 
-def determine_longest_path_length(hiking_map: dict[Tuple[int, int], str]) -> int:
-    path_lengths: List[int] = []
+def find_traversable_neighbors(position: Tuple[int, int], hiking_map: dict[Tuple[int, int], str], minding_slopes: bool) -> Sequence[Tuple[int, int]]:
+    traversable_neighbors: Sequence[Tuple[int, int]] = []
+    x, y = position
+    for dx, dy in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
+        nx, ny = x + dx, y + dy
+        next_position = hiking_map.get((nx, ny))
+        if next_position and next_position != "#":
+            if not minding_slopes:
+                traversable_neighbors.append((nx, ny))
+            else:
+                if (nx < x and next_position != ">") or (nx > x and next_position != "<") or (ny < y and next_position != "v") or (ny > y and next_position != "^"):
+                    traversable_neighbors.append((nx, ny))
+    return traversable_neighbors
+
+
+def determine_longest_path_length(hiking_map: dict[Tuple[int, int], str], minding_slopes: bool) -> int:
+    longest_path_length = 0
     start_and_end = find_start_and_end(hiking_map)
     start = start_and_end["start"]
     end = start_and_end["end"]
     stack: Sequence[Sequence[Tuple[int, int]]] = [[start]]
     while len(stack):
+        print('STACK LENGTH:', len(stack))
+        print('CURRENT MAX LENGTH:', longest_path_length)
         path = stack.pop()
         current_location = path[-1]
-        if current_location == end:
-            path_lengths.append(len(path) - 1)
+        if current_location == end and len(path) - 1 > longest_path_length:
+            longest_path_length = len(path) - 1
         else:
-            neighbors = find_traversable_neighbors(current_location, hiking_map)
+            neighbors = find_traversable_neighbors(current_location, hiking_map, minding_slopes)
             for neighbor in neighbors:
                 if neighbor not in path:
                     stack.append([*path, neighbor])
-    return sorted(path_lengths)[-1]
+    return longest_path_length
 
 
 def solve_problem(is_official: bool) -> SolutionResults:
     start_time = time.time()
     data = extract_data_from_file(23, is_official)
     hiking_map = create_hiking_map(data)
-    part_1 = determine_longest_path_length(hiking_map)
-    part_2 = 2 if data else 0
+    part_1 = determine_longest_path_length(hiking_map, True)
+    # EXCEPTION: Brute-force technique requires more than 2 days to execute with full data, and even then it doesn't finish, it merely has found the longest path (not verified it)
+    part_2 = determine_longest_path_length(hiking_map, False)
     end_time = time.time()
     execution_time = end_time - start_time
     results = SolutionResults(23, part_1, part_2, execution_time)
--- day_24/solution.py
+++ day_24/solution.py
@@ -1,12 +1,95 @@
 import time
+from typing import List
+from utils.get_list_of_lines import get_list_of_lines
 from utils.extract_data_from_file import extract_data_from_file
 from utils.SolutionResults import SolutionResults
+
+
+def determine_linear_equation_elements(data: str) -> List[List[int]]:
+    lines = get_list_of_lines(data)
+    linear_equation_elements: List[List[int]] = []
+    for line in lines:
+        trimmed = line.replace(" ", "")
+        parts = trimmed.split("@")
+        constants = parts[0].split(",")
+        velocities = parts[1].split(",")
+        xc = int(constants[0])
+        yc = int(constants[1])
+        xv = int(velocities[0])
+        yv = int(velocities[1])
+        linear_equation_elements.append([xc, yc, xv, yv])
+    return linear_equation_elements
+
+
+def calculate_2d_matrix_inverse(matrix: List[List[int]]) -> List[List[float]] | None:
+    a = matrix[0][0]
+    b = matrix[0][1]
+    c = matrix[1][0]
+    d = matrix[1][1]
+    determinant = a * d - b * c
+    if determinant:
+        reciprocal = 1 / determinant
+        top = [d * reciprocal, -1 * b * reciprocal]
+        bottom = [-1 * c * reciprocal, a * reciprocal]
+        return [top, bottom]
+    else:
+        return None
+
+
+def determine_2d_intersection_point(coefficients: List[List[int]], constants: List[int]) -> List[float] | None:
+    inverse = calculate_2d_matrix_inverse(coefficients)
+    if inverse:
+        a = inverse[0][0]
+        b = inverse[0][1]
+        c = inverse[1][0]
+        d = inverse[1][1]
+        e = constants[0]
+        f = constants[1]
+        x = a * e + b * f
+        y = c * e + d * f
+        return [x, y]
+    else:
+        return None
+
+
+def find_all_intersections_within_interval(equation_elements: List[List[int]]) -> int:
+    total = 0
+    min = 200000000000000
+    max = 400000000000000
+    for index in range(len(equation_elements) - 1):
+        first_equation_elements = equation_elements[index]
+        xc1 = first_equation_elements[0]
+        yc1 = first_equation_elements[1]
+        xv1 = first_equation_elements[2]
+        yv1 = first_equation_elements[3]
+        first_coefficients = [yv1, -1 * xv1]
+        first_constant = yv1 * xc1 - xv1 * yc1
+        for second_index in range(index + 1, len(equation_elements)):
+            second_equation_elements = equation_elements[second_index]
+            xc2 = second_equation_elements[0]
+            yc2 = second_equation_elements[1]
+            xv2 = second_equation_elements[2]
+            yv2 = second_equation_elements[3]
+            second_coefficients = [yv2, -1 * xv2]
+            second_constant = yv2 * xc2 - xv2 * yc2
+            coefficients = [first_coefficients, second_coefficients]
+            constants = [first_constant, second_constant]
+            intersection_point = determine_2d_intersection_point(coefficients, constants)
+            if intersection_point:
+                x = intersection_point[0]
+                y = intersection_point[1]
+                t1 = (x - xc1) / xv1
+                t2 = (x - xc2) / xv2
+                if min < x < max and min < y < max and t1 > 0 and t2 > 0:
+                    total += 1
+    return total
 
 
 def solve_problem(is_official: bool) -> SolutionResults:
     start_time = time.time()
     data = extract_data_from_file(24, is_official)
-    part_1 = 1 if data else 0
+    equation_elements = determine_linear_equation_elements(data)
+    part_1 = find_all_intersections_within_interval(equation_elements)
     part_2 = 2 if data else 0
     end_time = time.time()
     execution_time = end_time - start_time
