60df4f6437fe66b3034f3da52c80a34c45b257ed
Jackson Reeves <jackson.reeves@slalom.com>
2024-01-01 01:16:12 -0500

    Add expansion method to solve part 2 for day 21

--- day_21/solution.py
+++ day_21/solution.py
@@ -5,12 +5,17 @@ from utils.extract_data_from_file import extract_data_from_file
 from utils.SolutionResults import SolutionResults
 
 
-class Map:
+class FarmMap:
     def __init__(self, description: str) -> None:
         self.rows = get_list_of_lines(description)
         self.height = self.calculate_height()
         self.width = self.calculate_width()
         self.positions = self.create_positions()
+        self.core_positions = self.positions.copy()
+        self.y_min = 0
+        self.y_max = self.height - 1
+        self.x_min = 0
+        self.x_max = self.width - 1
 
     def calculate_height(self) -> int:
         return len(self.rows)
@@ -36,10 +41,11 @@ class Map:
         possible_coordinates = [(x + dx, y + dy) for dx, dy in [(-1, 0), (1, 0), (0, -1), (0, 1)]]
         for coordinates in possible_coordinates:
             new_x, new_y = coordinates
-            if 0 <= new_x < self.width and 0 <= new_y < self.height:
-                possible_plot = self.positions.get(coordinates)
-                if possible_plot:
-                    possible_positions.append(coordinates)
+            if not self.x_min <= new_x <= self.x_max or not self.y_min <= new_y <= self.y_max:
+                self.expand_positions()
+            possible_plot = self.positions.get(coordinates)
+            if possible_plot:
+                possible_positions.append(coordinates)
         return possible_positions
 
     def find_all_possible_positions_after_step_with_multiple_starts(self, starting_positions: Set[Tuple[int, int]]) -> Set[Tuple[int, int]]:
@@ -57,13 +63,31 @@ class Map:
             current_positions = self.find_all_possible_positions_after_step_with_multiple_starts(current_positions)
         return len(current_positions)
 
+    def expand_positions(self) -> None:
+        current_height = self.y_max - self.y_min
+        current_width = self.x_max - self.x_min
+        self.y_max += current_height
+        self.y_min -= current_height
+        self.x_max += current_width
+        self.x_min -= current_width
+        shifts = [(dx, dy) for dx in range(-current_width, current_width + 1) for dy in range(-current_height, current_height + 1) if (dx, dy) != (0, 0)]
+        new_maps: List[dict[Tuple[int, int], bool]] = []
+        for shift in shifts:
+            shifted_map = {}
+            for coordinates, value in self.positions.items():
+                shifted_coordinates = tuple(x + dx for x, dx in zip(coordinates, shift))
+                shifted_map[shifted_coordinates] = value
+            new_maps.append(shifted_map)
+        for new_map in new_maps:
+            self.positions.update(new_map)
+
 
 def solve_problem(is_official: bool) -> SolutionResults:
     start_time = time.time()
     data = extract_data_from_file(21, is_official)
-    map = Map(data)
-    steps = 64 if is_official else 6
-    part_1 = map.determine_reachable_plots_after_certain_steps(steps)
+    farm = FarmMap(data)
+    steps = 64 if is_official else 10
+    part_1 = farm.determine_reachable_plots_after_certain_steps(steps)
     part_2 = 2 if data else 0
     end_time = time.time()
     execution_time = end_time - start_time
