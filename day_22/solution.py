57e3b3e3defc95dee57f0652431a60ef648bdb5c
Jackson Reeves <jackson.reeves@slalom.com>
2024-01-06 18:15:04 -0500

    Count bricks could remove

--- day_22/solution.py
+++ day_22/solution.py
@@ -1,17 +1,21 @@
 import time
-from typing import Tuple, List
+from typing import Tuple, Set, List
+from queue import Queue
 from utils.get_list_of_lines import get_list_of_lines
 from utils.extract_data_from_file import extract_data_from_file
 from utils.SolutionResults import SolutionResults
 
 
 class Brick:
-    def __init__(self, snapshot: str) -> None:
+    def __init__(self, name: str, snapshot: str) -> None:
         ends = snapshot.split("~")
+        self.name = name
         self.start = self.create_coordinates(ends[0])
         self.end = self.create_coordinates(ends[1])
         self.orientation = self.determine_orientation()
         self.length = self.determine_length()
+        self.beneath_bricks: Set[str] = set()
+        self.above_bricks: Set[str] = set()
 
     def create_coordinates(self, description: str) -> Tuple[int, ...]:
         parts = description.split(",")
@@ -29,17 +33,17 @@ class Brick:
         length = self.end[self.orientation] - self.start[self.orientation]
         return length
 
-    def get_all_cubes(self) -> List[Tuple[int, ...]]:
-        cubes: List[Tuple[int, ...]] = []
+    def create_all_cubes(self) -> List[Tuple[int, int, int, str]]:
+        cubes: List[Tuple[int, int, int, str]] = []
         orientation = self.orientation
         length = self.length
         for index in range(length):
             if orientation == 0:
-                cubes.append((self.start[0] + index, self.start[1], self.start[2]))
+                cubes.append((self.start[0] + index, self.start[1], self.start[2], self.name))
             if orientation == 1:
-                cubes.append((self.start[0], self.start[1] + index, self.start[2]))
+                cubes.append((self.start[0], self.start[1] + index, self.start[2], self.name))
             if orientation == 2:
-                cubes.append((self.start[0], self.start[1], self.start[2] + index))
+                cubes.append((self.start[0], self.start[1], self.start[2] + index, self.name))
         return cubes
 
 
@@ -47,100 +51,135 @@ class Stack:
     def __init__(self, description: str) -> None:
         snapshots = get_list_of_lines(description)
         self.bricks = self.create_bricks(snapshots)
-        self.dimensions = self.determine_dimensions()
-        self.points = self.create_points()
-        self.time = 0
+        self.bricks_queue = self.create_bricks_queue()
+        self.dimensions = self.determine_base_dimensions()
+        self.base = self.create_base()
 
-    def create_bricks(self, snapshots: List[str]) -> List[Brick]:
-        initial_bricks: List[Brick] = []
-        for snapshot in snapshots:
-            brick = Brick(snapshot)
-            initial_bricks.append(brick)
-        sorted_bricks = sorted(initial_bricks, key=lambda x: x.start[2])
-        return sorted_bricks
+    def create_bricks(self, snapshots: List[str]) -> dict[str, Brick]:
+        bricks: dict[str, Brick] = {}
+        for index in range(len(snapshots)):
+            snapshot = snapshots[index]
+            name = f"B{index}"
+            brick = Brick(name, snapshot)
+            bricks[name] = brick
+        return bricks
 
-    def determine_dimensions(self) -> Tuple[int, ...]:
+    def create_bricks_queue(self) -> Queue[List[Tuple[int, int, int, str]]]:
+        bricks_to_drop: List[List[Tuple[int, int, int, str]]] = []
+        for brick in self.bricks.values():
+            cubes = brick.create_all_cubes()
+            bricks_to_drop.append(cubes)
+        sorted_bricks = sorted(bricks_to_drop, key=lambda x: x[0][2])
+        bricks_queue: Queue[List[Tuple[int, int, int, str]]] = Queue()
+        for brick in sorted_bricks:
+            bricks_queue.put(brick)
+        return bricks_queue
+
+    def determine_base_dimensions(self) -> Tuple[int, ...]:
         x_max = 0
         y_max = 0
-        z_max = 0
-        for brick in self.bricks:
-            x, y, z = brick.start
+        for brick in self.bricks.values():
+            x, y = brick.start
             orientation = brick.orientation
             length = brick.length
             if orientation == 0:
                 x += length
             if orientation == 1:
                 y += length
-            if orientation == 2:
-                z += length
             if x > x_max:
                 x_max = x
             if y > y_max:
                 y_max = y
-            if z > z_max:
-                z_max = z
-        return x_max + 1, y_max + 1, z_max + 1
+        return x_max, y_max
 
-    def create_points(self) -> dict[Tuple[int, ...], bool]:
-        points: dict[Tuple[int, ...], bool] = {}
-        x_max, y_max, z_max = self.dimensions
+    def create_base(self) -> dict[Tuple[int, ...], str]:
+        points: dict[Tuple[int, ...], str] = {}
+        x_max, y_max = self.dimensions
         for x in range(x_max):
             for y in range(y_max):
-                for z in range(z_max):
-                    points[x, y, z] = False
-        for brick in self.bricks:
-            cubes = brick.get_all_cubes()
-            for cube in cubes:
-                points[cube] = True
+                points[(x, y, 0)] = ""
         return points
 
-    def move_bricks_down_once(self) -> int:
-        total_bricks_moved = 0
-        for brick in self.bricks:
+    def move_bricks_down_once(self) -> None:
+        updated_queue: Queue[List[Tuple[int, int, int, str]]] = Queue()
+        while not self.bricks_queue.empty():
+            brick = self.bricks_queue.get()
             can_move_down = self.check_if_brick_can_move_down(brick)
             if can_move_down:
-                total_bricks_moved += 1
-                cubes = brick.get_all_cubes()
-                for cube in cubes:
-                    self.points[cube] = False
-                for cube in cubes:
-                    self.points[cube[0], cube[1], cube[2] - (1 + self.time)] = True
-        return total_bricks_moved
+                new_brick: List[Tuple[int, int, int, str]] = []
+                for cube in brick:
+                    x0, y0, z0, name = cube
+                    x1, y1, z1 = x0, y0, z0 - 1
+                    new_brick.append((x1, y1, z1, name))
+                updated_queue.put(new_brick)
+            else:
+                full_brick = self.bricks[brick[0][3]]
+                for cube in brick:
+                    x, y, z, name = cube
+                    self.base[(x, y, z)] = name
+                    potential_below = self.base.get((x, y, z - 1))
+                    if potential_below:
+                        full_brick.above_bricks.add(potential_below)
+                        full_below_block = self.bricks[potential_below]
+                        full_below_block.beneath_bricks.add(full_brick.name)
+        self.bricks_queue = updated_queue
 
     def move_all_bricks_down_to_ground(self) -> None:
-        can_any_bricks_move = True
-        while can_any_bricks_move:
-            self.time += 1
-            total_bricks_moved = self.move_bricks_down_once()
-            can_any_bricks_move = True if total_bricks_moved != 0 else False
+        while not self.bricks_queue.empty():
+            self.move_bricks_down_once()
 
-    def check_if_brick_can_move_down(self, brick: Brick) -> bool:
+    def check_if_brick_can_move_down(self, brick: List[Tuple[int, int, int, str]]) -> bool:
         can_move_down = True
-        if brick.orientation == 2:
-            x, y, z = brick.start
-            if z - (1 + self.time) == 1:
+        for cube in brick:
+            if not self.check_if_cube_can_move_down(cube):
                 can_move_down = False
-            else:
-                can_move_down = not self.points[x, y, z - (1 + self.time)]
-        else:
-            cubes = brick.get_all_cubes()
-            for cube in cubes:
-                x, y, z = cube
-                if z - (1 + self.time) == 1 or self.points[x, y, z - (1 + self.time)]:
-                    can_move_down = False
         return can_move_down
+
+    def check_if_cube_can_move_down(self, cube: Tuple[int, int, int, str]) -> bool:
+        can_move_down = True
+        x0, y0, z0, _ = cube
+        x1, y1, z1 = x0, y0, z0 - 1
+        if z1 == 0:
+            can_move_down = False
+        else:
+            if self.base.get((x1, y1, z1)):
+                can_move_down = False
+        return can_move_down
+
+    def check_if_brick_can_be_removed(self, brick: Brick) -> bool:
+        can_be_removed = False
+        brick_count_above = len(brick.beneath_bricks)
+        if brick_count_above == 0:
+            can_be_removed = True
+        else:
+            above_bricks_covered_elsewhere = 0
+            for above_brick in brick.beneath_bricks:
+                full_brick = self.bricks[above_brick]
+                if len(full_brick.above_bricks) >= 2:
+                    above_bricks_covered_elsewhere += 1
+            if above_bricks_covered_elsewhere == brick_count_above:
+                can_be_removed = True
+        return can_be_removed
+
+    def count_total_bricks_could_remove(self) -> int:
+        total = 0
+        for brick in self.bricks.values():
+            can_be_removed = self.check_if_brick_can_be_removed(brick)
+            if can_be_removed:
+                total += 1
+        return total
 
 
 def solve_problem(is_official: bool) -> SolutionResults:
     start_time = time.time()
     data = extract_data_from_file(22, is_official)
     stack = Stack(data)
-    print(stack.points)
-    print(stack.dimensions)
-    print(len(stack.points.keys()))
+    # print(stack.base)
+    # print(stack.dimensions)
+    # print(len(stack.base.keys()))
     stack.move_all_bricks_down_to_ground()
-    print(stack.points)
-    part_1 = 1 if data else 0
+    # print(stack.base)
+    part_1 = stack.count_total_bricks_could_remove()
     part_2 = 2 if data else 0
     end_time = time.time()
     execution_time = end_time - start_time
