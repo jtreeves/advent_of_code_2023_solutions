fd5f48200379d3db5af81e6103458fab420ac1e2
Jackson Reeves <jackson.reeves@slalom.com>
2024-01-06 23:35:29 -0500

    Solve day 25

--- day_25/solution.py
+++ day_25/solution.py
@@ -1,5 +1,5 @@
 import time
-from typing import List, Set
+from typing import List, Set, Tuple, Callable
 from utils.get_list_of_lines import get_list_of_lines
 from utils.extract_data_from_file import extract_data_from_file
 from utils.SolutionResults import SolutionResults
@@ -15,13 +15,6 @@ class WiringDiagram:
     def __init__(self, description: str) -> None:
         component_descriptions = get_list_of_lines(description)
         self.components = self.create_components(component_descriptions)
-
-    def __repr__(self) -> str:
-        representation = ""
-        for key in sorted(self.components.keys()):
-            component = self.components[key]
-            representation += f"{component.name}: {component.connections}\n"
-        return representation
 
     def create_components(self, descriptions: List[str]) -> dict[str, Component]:
         components: dict[str, Component] = {}
@@ -44,25 +37,18 @@ class WiringDiagram:
                     components[connection] = new_connection
         return components
 
-    def find_partitions_with_stoer_wagner(self) -> List[Set[str]]:
-        initial_partition: Set[str] = set(self.components.keys())
-        new_partition: Set[str] = set()
-        while len(new_partition) < len(initial_partition) - 3:
-            min_cut_size = float("inf")
-            min_cut_node = ""
-            for component in initial_partition:
-                cut_size = sum(1 for neighbor in self.components[component].connections if neighbor in new_partition)
-                if cut_size < min_cut_size:
-                    min_cut_size = cut_size
-                    min_cut_node = component
-            initial_partition.remove(min_cut_node)
-            new_partition.add(min_cut_node)
-        return [initial_partition, new_partition]
+    def break_components_into_partitions(self) -> Tuple[Set[str], Set[str]]:
+        initial_components = set(self.components.keys())
+        count_external_edges: Callable[[str], int] = lambda component_name: len(set(self.components[component_name].connections) - initial_components)
+        while initial_components and sum(map(count_external_edges, initial_components)) != 3:
+            component_to_remove = max(initial_components, key=count_external_edges)
+            initial_components.remove(component_to_remove)
+        first_partition = initial_components
+        second_partition = set(self.components.keys()) - initial_components
+        return first_partition, second_partition
 
     def calculate_product_of_partition_sizes(self) -> int:
-        partitions = self.find_partitions_with_stoer_wagner()
-        first_partition = partitions[0]
-        second_partition = partitions[1]
+        first_partition, second_partition = self.break_components_into_partitions()
         product = len(first_partition) * len(second_partition)
         return product
 
@@ -72,7 +58,7 @@ def solve_problem(is_official: bool) -> SolutionResults:
     data = extract_data_from_file(25, is_official)
     diagram = WiringDiagram(data)
     part_1 = diagram.calculate_product_of_partition_sizes()
-    part_2 = 2 if data else 0
+    part_2 = 50
     end_time = time.time()
     execution_time = end_time - start_time
     results = SolutionResults(25, part_1, part_2, execution_time)
